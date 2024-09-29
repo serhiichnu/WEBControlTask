b7ad8dbcec8b4c3cc8426dd12d8d803dfb378e0e
serhiichnu <skurtul.serhii@chnu.edu.ua>
2024-09-29 22:33:18 +0300

    sidebar for mobile added

---
+++ index.js
@@ -0,0 +1,7 @@
+function showSidebar(){
+    document.getElementById('sidebar').style.display = 'block'
+}
+
+function hideSidebar(){
+    document.getElementById('sidebar').style.display = 'none'
+}
